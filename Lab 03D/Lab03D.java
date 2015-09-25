42b0dbb8b20801e9ebf8007bbf6401db60b06691
unknown <14268@HS222-05.vtsd.com>
2015-09-25 11:53:48 -0400

    first commit

---
+++ Lab 03D/Lab03D.java
@@ -0,0 +1,58 @@
+
+/**
+ * Write a description of class Lab03D here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Lab03D
+{
+    public static void main(String args[])
+	{
+		System.out.println("Lab 03D");
+		System.out.println();
+		int num1 = 11;
+		int num2 = 22;
+		int num3 = 33;
+		int num4 = 44;
+		int num5 = 55;
+		int total = 0;
+		int average = 0;
+		System.out.println("1st integer:" + num1);
+		System.out.println("2nd integer:" + num2);
+		System.out.println("3rd integer:" + num3);
+		System.out.println("4th integer:" + num4);
+		System.out.println("5th integer:" + num5);
+		System.out.println("----------------------");
+		
+		total = num1 + num2 + num3 + num4 + num5;
+		
+		average = total / 5;
+		
+		System.out.println("Int total:" + total);
+		System.out.println("int Average:" + average);
+		System.out.println();
+		System.out.println();
+		double nm1 = 55.555;
+		double nm2 = 66.666;
+		double nm3 = 77.777;
+		double nm4 = 88.888;
+		double nm5 = 99.999;
+		double Realtotal = 0;
+		double realaverage = 0;
+		Realtotal = nm1 + nm2 + nm3 + nm4 + nm5;
+		realaverage = Realtotal / 5;
+		System.out.println("1st real num:" + nm1);
+		System.out.println("2nd real num:" + nm2);
+		System.out.println("3rd real num:" + nm3);
+		System.out.println("4th real num:" + nm4);
+		System.out.println("5th real num:" + nm5);
+		System.out.println("----------------------");
+		System.out. println("Real Total" + Realtotal);
+		System.out.println("Real average" + realaverage);
+		
+		
+		System.out.println();
+}
+}
+		
---
+++ Lab03Dst.java
@@ -0,0 +1,24 @@
+// Lab03Dst.java
+// The Visible Average Program
+// This is the student, starting version, of Lab 03C.
+
+
+public class Lab03Dst
+{
+	public static void main(String args[])
+	{
+		System.out.println("Lab 03D");
+		System.out.println();
+		int num1 = 11;
+		int num2 = 22;
+		int num3 = 33;
+		int num4 = 44;
+		int num5 = 55;
+
+
+
+
+
+		System.out.println();
+	}
+}
